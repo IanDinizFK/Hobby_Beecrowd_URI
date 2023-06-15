512d974ae40c0852a7a230abf962535a1fdb00eb
Ian Diniz de Oliveira <51161747+IanDinizFK@users.noreply.github.com>
2023-06-15 00:43:15 -0300

    Create URI1041.c

---
+++ URI1041.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+  float x,y;
+  scanf("%f %f", &x,&y);
+  if(x == 0 && y == 0){
+    printf("Origem\n");
+    return 0;
+  }else if(x == 0 && y != 0){
+    printf("Eixo Y\n");
+    return 0;
+  }else if(x != 0 && y == 0){
+    printf("Eixo X\n");
+    return 0;
+  }else if(x > 0 && y > 0){
+    printf("Q1\n");
+    return 0;
+  }else if(x < 0 && y > 0){
+    printf("Q2\n");
+    return 0;
+  }else if(x > 0 && y < 0){
+    printf("Q4\n");
+    return 0;
+  }else if(x < 0 && y < 0){
+    printf("Q3\n");
+    return 0;
+  }
+}
