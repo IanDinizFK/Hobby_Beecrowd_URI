ca32132525a5c8328ea3c7afbe69aa476d2597aa
Ian Diniz de Oliveira <51161747+IanDinizFK@users.noreply.github.com>
2023-07-08 22:57:02 -0300

    Create 1114.c

---
+++ 1114.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    int i;
+    int a = 0;
+    while(a != 2002){
+        scanf("%d", &a);
+        if(a != 2002){
+            printf("Senha Invalida\n");
+        }else{
+            printf("Acesso Permitido\n");
+        }
+    }
+ 
+    return 0;
+}
